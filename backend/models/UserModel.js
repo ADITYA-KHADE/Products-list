f7999deb7770c5408d39ebc4a130219318738f34
adityakhade321 <adityakhadeak47@gmail.com>
2024-12-31 01:01:13 +0530

    update README.md with application overview, features, and setup instructions; format UserModel schema

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,28 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const UserModelSchema = new mongoose.Schema({
-    name:{
-        type: String,
-        required: true,
+const UserModelSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    email:{
-        type: String,
-        required: true,
-        unique: true,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    password:{
-        type: String,
-        required: true,
+    password: {
+      type: String,
+      required: true,
     },
-    role:{
-        type: String,
-        default: "user",
+    role: {
+      type: String,
+      default: "user",
     },
-
-},{
+  },
+  {
     timestamps: true,
-});
+  }
+);
 
-const UserModel = mongoose.model('User', UserModelSchema)
+const UserModel = mongoose.model("User", UserModelSchema);
 
-module.exports = UserModel
+module.exports = UserModel;
