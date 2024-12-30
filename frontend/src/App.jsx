eeac7140c41972a4771821988822ec0a1aad5a85
adityakhade321 <adityakhadeak47@gmail.com>
2024-12-30 23:25:51 +0530

    add Footer component and enhance App component with improved data fetching logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,48 @@
-import React from "react";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import Login from "./Pages/Auth/Login";
 import Signup from "./pages/Auth/Signup";
 import Home from "./pages/Home";
 import { Toaster } from "react-hot-toast";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { useAuthContext } from "./contexts/AuthContext";
-import ProductInfo from "./Components/ProductInfo/ProductInfo"
+import ProductInfo from "./Components/ProductInfo/ProductInfo";
 import "./App.css";
+import Footer from "./Components/Footer/Footer";
 
 const App = () => {
   const { authUser } = useAuthContext();
-  useEffect(() => {
 
+  useEffect(() => {
     const fetchData = async () => {
+      if (!authUser?._id) return; // Check if authUser and authUser._id are available
+
       try {
-        const response = await fetch('/api/auth/getToken',{
+        const response = await fetch('/api/auth/getToken', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
           },
-          body: JSON.stringify({ id:authUser._id}),
+          body: JSON.stringify({ id: authUser._id }),
         });
+
         const data = await response.json();
+
+        if (!response.ok) {
+          throw new Error(data.message || 'Failed to fetch data');
+        }
         console.log(data);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
     };
-    if(authUser){
+
+    if (authUser) {
       fetchData();
     }
-  }, []);
+  }, [authUser?._id]); // Dependency on authUser._id ensures the effect runs when the authUser changes
+
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <Routes>
         <Route
           path="/"
@@ -52,6 +61,7 @@ const App = () => {
           element={authUser ? <ProductInfo /> : <Navigate to="/login" />}
         />
       </Routes>
+      <Footer />
       <Toaster />
     </div>
   );
